de37758d9f4b3fc48acc1ad85eed7124eb634b80
avanfeel <wbunge@avanfeel.com>
2023-10-26 21:26:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,22 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
+const auth0LoginEndpoint = '';
+
 app.get('/auth', async (req, res) => {
-    // Extract login details from the request (e.g., username, password)
+
     const { username, password } = req.query;
 
-    // Forward the login request to Auth0
     try {
-        const response = await axios.post('YOUR_AUTH0_LOGIN_ENDPOINT', {
+        const response = await axios.post(auth0LoginEndpoint, {
             username,
             password,
-            // ... any other necessary Auth0 parameters
         });
 
-        // Handle the Auth0 response. If successful, you'll get an access token or ID token.
         const token = response.data.id_token;
 
-        // Forward the token to Shopify or handle the callback as needed
-        // ...
-
         res.send('Login successful!');
+
     } catch (error) {
         res.status(400).send('Login failed!');
     }
